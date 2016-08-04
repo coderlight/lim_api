71294aa9edcb97300e9586e4132d6f6f07af530c
coderlight <coder.light.os@gmail.com>
2016-08-04 16:54:00 +0300

    delete pending for spec

--- spec/models/h2_tag_spec.rb
+++ spec/models/h2_tag_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe H2Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/h3_tag_spec.rb
+++ spec/models/h3_tag_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe H3Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
